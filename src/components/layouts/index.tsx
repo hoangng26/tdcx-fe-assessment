6ac8bd83292aa3cee3748607cdd66de4ab76aebc
Nguyen Le Huu Hoang <hoangnguyenhk14cqt@gmail.com>
2025-02-14 09:43:06 +0700

    implement responsive header with navigation and logo; integrate styled-components for layout

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,14 +1,71 @@
+import { ReactIcon } from "@/assets";
 import React from "react";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+import styled from "styled-components";
+import { Button } from "../ui/button";
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 0;
+`;
+
+const LogoWrapper = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  .logo {
+    width: 24px;
+    height: 24px;
+  }
+
+  .text {
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+`;
 
 const RootLayout: React.FC = () => {
   return (
     <>
-      <header className="container">Header</header>
-      <main className="container">
+      <Header className="container">
+        <Link to="/">
+          <LogoWrapper>
+            <img src={ReactIcon} alt="Logo" className="logo" />
+
+            <span className="text">React App</span>
+          </LogoWrapper>
+        </Link>
+
+        <nav>
+          <ul className="flex space-x-8">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+
+            <li>
+              <Link to="/to-do">To do</Link>
+            </li>
+
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex space-x-4">
+          <Button variant="ghost">Login</Button>
+          <Button>Register</Button>
+        </div>
+      </Header>
+
+      <main className="container py-4">
         <Outlet />
       </main>
-      <footer className="container">Footer</footer>
+
+      {/* <footer className="container">Footer</footer> */}
     </>
   );
 };
